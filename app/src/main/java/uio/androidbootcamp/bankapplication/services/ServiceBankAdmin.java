3c87e8b7badc46062ebfa05af90aa3571e4cec04
Jorge Rodriguez <jorgerodriguez@MacBook-Pro-de-Jorge.local>
2018-12-11 19:53:44 -0500

    Creates clients

--- app/src/main/java/uio/androidbootcamp/bankapplication/services/ServiceBankAdmin.java
+++ app/src/main/java/uio/androidbootcamp/bankapplication/services/ServiceBankAdmin.java
@@ -5,8 +5,8 @@ import uio.androidbootcamp.bankapplication.entities.Client;
 public class ServiceBankAdmin {
     private Client client;
 
-    public void createClient() {
-        client = new Client("", "", "");
+    public void createClient(String name, String lastName, String id) {
+        client = new Client(name, lastName, id);
     }
 
     public Client getClient() {
--- app/src/test/java/uio/androidbootcamp/bankapplication/services/ServiceBankAdminTest.java
+++ app/src/test/java/uio/androidbootcamp/bankapplication/services/ServiceBankAdminTest.java
@@ -1,20 +1,53 @@
 package uio.androidbootcamp.bankapplication.services;
 
+import org.junit.Before;
 import org.junit.Test;
 
-import static org.hamcrest.CoreMatchers.not;
-import static org.hamcrest.CoreMatchers.nullValue;
+import static org.hamcrest.CoreMatchers.*;
 import static org.hamcrest.MatcherAssert.assertThat;
 
 public class ServiceBankAdminTest {
 
+    private ServiceBankAdmin serviceBankAdmin;
+
+    @Before
+    public void init(){
+        serviceBankAdmin = new ServiceBankAdmin();
+    }
+
 
     @Test
     public void testShouldCreateClient(){
-        ServiceBankAdmin serviceBankAdmin = new ServiceBankAdmin();
 
-        serviceBankAdmin.createClient();
+        serviceBankAdmin.createClient("", "", "");
 
         assertThat(serviceBankAdmin.getClient(), not(nullValue()));
+    }
+
+    @Test
+    public void testShouldCreateClientWithGivenName(){
+        String givenName = "Jaime";
+
+        serviceBankAdmin.createClient(givenName, "", "");
+
+        assertThat(serviceBankAdmin.getClient().getName(), is(givenName));
+    }
+
+    @Test
+    public void testShouldCreateClientWithGivenLastName(){
+        String lastName = "Borja";
+
+        serviceBankAdmin.createClient("",lastName, "");
+
+        assertThat(serviceBankAdmin.getClient().getLastName(), is(lastName));
+    }
+
+    @Test
+    public void testShouldCreateClientWithGivenId(){
+        String id = "12";
+
+        serviceBankAdmin.createClient("","", id);
+
+        assertThat(serviceBankAdmin.getClient().getId(), is(id));
     }
 }
